bd3f4dd5fd6548dc0a7da6fd9cf47c075b3aa509
Eron Moraes <eron.moraes7@gmail.com>
2022-09-07 17:34:11 -0300

    .

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump'); 
@@ -7,5 +8,15 @@ const jump = () => {
         mario.classList.remove('jump'); 
     }, 500);
 }
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.OffsetLeft;
+
+    if (pipePosition <= 120) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    }
+
+}, 10)
 
 document.addEventListener('keydown', jump);
