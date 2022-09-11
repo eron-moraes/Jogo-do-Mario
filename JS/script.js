7d01fd26a837b1af7de442b7fe939e575fc01fe1
Eron Moraes <eron.moraes7@gmail.com>
2022-09-11 01:27:29 -0300

    versao final

--- JS/script.js
+++ JS/script.js
@@ -2,27 +2,25 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
 const jump = () => {
-    mario.classList.add('jump'); 
+    mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump'); 
-    }, 500);
+        mario.classList.remove('jump');
+    },500);
 }
 
 const loop = setInterval(() => {
 
-    console.log('loop')
+    console.log('loop');
 
-    const pipePosition = pipe.OffsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom.replace('px','');
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
-    console.log(marioPosition);
-
-    if (pipePosition <= 120 && pipePosition > 0 && pipePosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-
+        
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
@@ -33,6 +31,6 @@ const loop = setInterval(() => {
         clearInterval(loop);
     }
 
-}, 10);
+},10);
 
 document.addEventListener('keydown', jump);
